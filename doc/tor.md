421cad8436b8e4fcc41a43a4d50b3b006b6bd593
Cameron <mcgreevycameron@gmail.com>
2020-11-09 17:57:56 +0000

    Changing capitalized names

--- doc/tor.md
+++ doc/tor.md
@@ -1,4 +1,4 @@
-# TOR SUPPORT IN LITECOIN
+# TOR SUPPORT IN CAMCOIN
 
 It is possible to run Camcoin Core as a Tor hidden service, and connect to such services.
 
--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -141,8 +141,8 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
         config = configparser.ConfigParser()
         config.read_file(open(self.options.configfile))
         self.config = config
-        self.options.bitcoind = os.getenv("LITECOIND", default=config["environment"]["BUILDDIR"] + '/src/camcoind' + config["environment"]["EXEEXT"])
-        self.options.bitcoincli = os.getenv("LITECOINCLI", default=config["environment"]["BUILDDIR"] + '/src/camcoin-cli' + config["environment"]["EXEEXT"])
+        self.options.bitcoind = os.getenv("CAMCOIND", default=config["environment"]["BUILDDIR"] + '/src/camcoind' + config["environment"]["EXEEXT"])
+        self.options.bitcoincli = os.getenv("CAMCOINCLI", default=config["environment"]["BUILDDIR"] + '/src/camcoin-cli' + config["environment"]["EXEEXT"])
 
         os.environ['PATH'] = os.pathsep.join([
             os.path.join(config['environment']['BUILDDIR'], 'src'),
