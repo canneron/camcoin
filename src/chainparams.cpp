d28424d2c89a14be8b0ddf1946538b897ffc79bb
Cameron <mcgreevycameron@gmail.com>
2020-11-10 20:42:20 +0000

    Changing port

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -105,7 +105,7 @@ public:
         pchMessageStart[1] = 0x55;
         pchMessageStart[2] = 0xb2;
         pchMessageStart[3] = 0x14;
-        nDefaultPort = 9333;
+        nDefaultPort = 9356;
         nPruneAfterHeight = 100000;
         m_assumed_blockchain_size = 22;
         m_assumed_chain_state_size = 3;
