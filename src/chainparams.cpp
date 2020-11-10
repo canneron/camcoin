fa7d147167d1033bdc73c6f7b743227e0f7ef1f3
Cameron <mcgreevycameron@gmail.com>
2020-11-10 20:30:19 +0000

    Removing LTC dnsseeds

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -120,10 +120,10 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("seed-a.camcoin.loshan.co.uk");
-        vSeeds.emplace_back("dnsseed.thrasher.io");
-        vSeeds.emplace_back("dnsseed.camcointools.com");
-        vSeeds.emplace_back("dnsseed.camcoinpool.org");
+        //vSeeds.emplace_back("seed-a.camcoin.loshan.co.uk");
+        //vSeeds.emplace_back("dnsseed.thrasher.io");
+        //vSeeds.emplace_back("dnsseed.camcointools.com");
+        //vSeeds.emplace_back("dnsseed.camcoinpool.org");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,27);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,27);
@@ -223,9 +223,9 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
         // nodes with support for servicebits filtering should be at the top
-        vSeeds.emplace_back("testnet-seed.camcointools.com");
-        vSeeds.emplace_back("seed-b.camcoin.loshan.co.uk");
-        vSeeds.emplace_back("dnsseed-testnet.thrasher.io");
+        //vSeeds.emplace_back("testnet-seed.camcointools.com");
+        //vSeeds.emplace_back("seed-b.camcoin.loshan.co.uk");
+        //vSeeds.emplace_back("dnsseed-testnet.thrasher.io");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
